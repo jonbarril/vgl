daee95a657e174b447f3c50bcaae6f955b92246a
Jon Barrilleaux <jonb@jmbaai.com>
2025-11-19 16:22:21 -0600

    initial

--- src/test/java/com/vgl/cli/CommitAndDiffTest.java
+++ src/test/java/com/vgl/cli/CommitAndDiffTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.nio.file.*;
-import java.util.*;
+//import java.util.*;
 
 public class CommitAndDiffTest {
 
