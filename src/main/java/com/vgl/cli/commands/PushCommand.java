d03c71bfda19034362eef44d7e94a289b7de55b6
Jon Barrilleaux <jonb@jmbaai.com>
2025-12-01 18:10:20 -0600

    Fix push command: use -noop flag instead of -dr

--- src/main/java/com/vgl/cli/commands/PushCommand.java
+++ src/main/java/com/vgl/cli/commands/PushCommand.java
@@ -11,7 +11,7 @@ public class PushCommand implements Command {
     @Override public String name(){ return "push"; }
 
     @Override public int run(List<String> args) throws Exception {
-        boolean dr = args.contains("-dr");
+        boolean noop = args.contains("-noop");
         try (Git git = Utils.openGit()) {
             if (git == null) {
                 System.out.println("Warning: No local repository found in: " + 
@@ -21,7 +21,7 @@ public class PushCommand implements Command {
             String branch = git.getRepository().getBranch();
             String remoteUrl = git.getRepository().getConfig().getString("remote","origin","url");
             if (remoteUrl == null) { System.out.println("No remote connected."); return 1; }
-            if (dr) { System.out.println("(dry run) would push local branch '" + branch + "' to remote: " + remoteUrl); return 0; }
+            if (noop) { System.out.println("(dry run) would push local branch '" + branch + "' to remote: " + remoteUrl); return 0; }
             git.push().setRemote("origin").setRefSpecs(new RefSpec(branch+":"+branch)).setForce(false).call();
             System.out.println("Pushed local branch '" + branch + "' to remote: " + remoteUrl);
         }
